c7cc8eb20e39cf17de9d520089b682d244ad54b8
Olivier Huin <olih@users.noreply.github.com>
2026-03-04 03:21:59 +0000

    Improve examples

--- design/write_examples.ts
+++ design/write_examples.ts
@@ -1,5 +1,12 @@
 import { writeFile } from 'node:fs/promises';
-import { exampleUseCase } from './examples.ts';
+import {
+  exampleH2Section,
+  exampleH3Section,
+  exampleNote,
+  exampleRelationship,
+  exampleReport,
+  exampleUseCase,
+} from './examples.ts';
 
 const EXAMPLES_PATH = 'doc/EXAMPLES.md';
 
@@ -7,10 +14,45 @@ export const generateExamplesReport = async () => {
   const lines: string[] = [];
   lines.push('# Examples (Generated)');
   lines.push('');
+  lines.push('## Note');
+  lines.push('');
+  lines.push('```json');
+  lines.push(exampleNote);
+  lines.push('```');
+  lines.push('');
+
   lines.push('## Use case');
   lines.push('');
   lines.push('```json');
   lines.push(exampleUseCase);
+  lines.push('```');
+  lines.push('');
+
+  lines.push('## Relationship');
+  lines.push('');
+  lines.push('```json');
+  lines.push(exampleRelationship);
+  lines.push('```');
+  lines.push('');
+
+  lines.push('## H3 section');
+  lines.push('');
+  lines.push('```json');
+  lines.push(exampleH3Section);
+  lines.push('```');
+  lines.push('');
+
+  lines.push('## H2 section');
+  lines.push('');
+  lines.push('```json');
+  lines.push(exampleH2Section);
+  lines.push('```');
+  lines.push('');
+
+  lines.push('## Report');
+  lines.push('');
+  lines.push('```json');
+  lines.push(exampleReport);
   lines.push('```');
   lines.push('');
 
